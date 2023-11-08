a08d2e0462a80bd188ce19afae3d5ec913179e2b
calorenzo21 <celorenzo2104@gmail.com>
2023-11-08 08:58:48 -0400

    changing base url

--- src/context/socketContext.js
+++ src/context/socketContext.js
@@ -8,7 +8,7 @@ export const SocketContext = createContext()
 
 export const SocketProvider = ({ children }) => {
 
-    const { socket, online, connectSocket, disconnectSocket } = useSocket('http://localhost:8080')
+    const { socket, online, connectSocket, disconnectSocket } = useSocket('https://ucabdocs-api-23cccedce5d8.herokuapp.com')
 
     const { auth } = useContext(AuthContext)
 
